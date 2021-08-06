487698873f700e41ff82ef170cc6fafa15537810
Sohail Irfan <thesohailirfan@gmail.com>
2021-08-06 20:07:36 +0530

    Added Navigation Tab Icons

--- screens/home/home.js
+++ screens/home/home.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import firebase from 'firebase';
 
 
@@ -36,10 +37,27 @@ function JourneyScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
-  return (
-      <Tab.Navigator>
-            <Tab.Screen name="journey" component={JourneyScreen} />
-            <Tab.Screen name="explore" component={ExploreScreen} />
-      </Tab.Navigator>
-  );
-}
+    return (
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Journey') {
+                iconName = focused ? 'journal' : 'journal-outline';
+              } else if (route.name === 'Explore') {
+                iconName = focused ? 'search-circle' : 'search-circle-outline';
+              }
+  
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+            <Tab.Screen name="Journey" component={JourneyScreen} />
+            <Tab.Screen name="Explore" component={ExploreScreen} />
+        </Tab.Navigator>
+    );
+  }
