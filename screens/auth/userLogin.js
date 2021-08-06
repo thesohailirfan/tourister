bf33df992993da1a29355870147360a86755233a
Ammobag <prithwishdey0321@gmail.com>
2021-08-06 21:36:00 +0530

    created theme file

--- screens/auth/userLogin.js
+++ screens/auth/userLogin.js
@@ -1,49 +1,61 @@
-import * as React from 'react';
-import { Text, View, Button, TextInput} from 'react-native';
-import firebase from 'firebase';
+import * as React from "react";
+import { Text, View, Button, TextInput, StyleSheet } from "react-native";
+import firebase from "firebase";
+import theme from "../Assets/theme";
 
-export default function LoginScreen({navigation}) {
-    const [email, setemail] = React.useState();
-    const [password, setpassword] = React.useState();
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = React.useState();
+  const [password, setPassword] = React.useState();
 
-    function handleSignIn(params) {
-        console.log("SignIn")
-        if(email && password) {
-            firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Signed in 
-                var user = userCredential.user;
-                console.log(user)
-            })
-            .catch((error) => {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.log(errorMessage)
-            });
-        }
+  function handleSignIn(params) {
+    console.log("SignIn");
+    if (email && password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          // Signed in
+          var user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorMessage);
+        });
     }
+  }
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <TextInput
-                style={{ height: 40, width: 300 , borderColor: 'gray', borderWidth: 1, textAlign: 'center' }}
-                onChangeText={text => setemail(text)}
-                value={email}
-                placeholder={"Email"}
-            />
-            <TextInput
-                style={{ height: 40, width: 300 ,borderColor: 'gray', borderWidth: 1, textAlign: 'center' }}
-                onChangeText={text => setpassword(text)}
-                value={password}
-                placeholder={"Password"}
-            />
+  return (
+    <View style={styles.main}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        placeholder={"Email"}
+      />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        placeholder={"Password"}
+      />
 
-            <Button
-                onPress={handleSignIn}
-                title="Sign In"
-                color="#841584"
-            />
-
-        </View>
-    );
+      <Button onPress={handleSignIn} title="Sign In" style={styles.signInBtn} />
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  main: { flex: 1, justifyContent: "center", alignItems: "center" },
+  textInput: {
+    height: 40,
+    width: 300,
+    borderColor: "gray",
+    borderWidth: 1,
+    textAlign: "center",
+  },
+  signInBtn: {
+    backgroundColor: "#000000",
+    color: "#ffffff",
+  },
+});
